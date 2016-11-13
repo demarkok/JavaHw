4593ce10638d5add75cccc93325a3c896480b941
Ilya Kaysin <demarkok@gmail.com>
2016-11-13 14:36:30 +0300

    complete SecondPartTask

--- Homework4/src/main/java/ru/spbau/kaysin/SecondPartTasks.java
+++ Homework4/src/main/java/ru/spbau/kaysin/SecondPartTasks.java
@@ -1,7 +1,12 @@
 package ru.spbau.kaysin;
 
+import java.awt.geom.Point2D;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,25 +14,45 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream()
+                .filter(s -> s.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
     // Стрелок атакует мишень и каждый раз попадает в произвольную точку квадрата.
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать, какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final int N = 1_000_000;
+
+        Random random = new Random();
+        return Stream.generate(() -> new Point2D.Double(random.nextDouble(), random.nextDouble()))
+            .mapToInt(point -> point.distance(0.5, 0.5) < 0.5 ? 1 : 0)
+                .limit(N)
+                .average()
+                .orElse(0);
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+                .max(Comparator.comparingInt(
+                        entry -> entry.getValue().stream()
+                                .mapToInt(String::length)
+                                .sum()))
+                .map(Map.Entry::getKey)
+                .orElse("");
+
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream()
+                .flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.summingInt(Map.Entry::getValue)));
     }
 }
--- Homework4/src/test/java/sp/SecondPartTasksTest.java
+++ Homework4/src/test/java/sp/SecondPartTasksTest.java
@@ -1,28 +1,77 @@
 package sp;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
+import static ru.spbau.kaysin.SecondPartTasks.*;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        assertEquals(
+                Arrays.asList("One", "Three", "Five"),
+                findQuotes(Arrays.asList("One", "Two", "Three", "Four", "Five"), "e")
+        );
+
+        assertEquals(
+                Arrays.asList("One", "Two", "Three"),
+                findQuotes(Arrays.asList("One", "Two", "Three"), "")
+        );
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        final double epsilon = 1e-2;
+        assertTrue(Math.abs(piDividedBy4() - Math.PI / 4.0) < epsilon);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        assertEquals(
+                "Kavinsky",
+                findPrinter(
+                        ImmutableMap.of(
+                                "Kavinsky", Arrays.asList("I'm giving you a nightcall to tell you how I feel",
+                                                          "I want to drive you through the night, down the hills"),
+
+                                "Michael Stipe", Arrays.asList("That's me in the corner",
+                                                               "That's me in the spotlight",
+                                                                "Losing my religion"),
+
+                                "Me", Arrays.asList("", "", "", "")
+                        )
+                )
+        );
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        assertEquals(
+                ImmutableMap.of(
+                        "Bags_of_grass", 2,
+                        "Pellets_of_mescaline", 75,
+                        "Sheets_of_blotter_acid", 5),
+                calculateGlobalOrder(Arrays.asList(
+                        ImmutableMap.of(
+                                "Bags_of_grass", 1,
+                                "Pellets_of_mescaline", 20,
+                                "Sheets_of_blotter_acid", 2
+                        ),
+                        ImmutableMap.of(
+                                "Bags_of_grass", 1,
+                                "Sheets_of_blotter_acid", 3
+                        ),
+                        ImmutableMap.of(
+                                "Pellets_of_mescaline", 20
+                        ),
+                        ImmutableMap.of(
+                                "Pellets_of_mescaline", 35
+                        )
+                ))
+        );
     }
 }
